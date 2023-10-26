7c55d53d735ffde20cb87cac56785c9abe6cc9cb
Aristarchos Tzatzos <djaris97@hotmail.com>
2023-10-27 00:27:26 +0300

    Reading Excel Working

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-const { app, BrowserWindow } = require('electron')
-const initSqlJs = require('sql.js');
+const { app, BrowserWindow } = require("electron");
+const initSqlJs = require("sql.js");
+const reader = require("xlsx");
+const file = reader.readFile("./TUCres.xlsx");
+let data = [];
+
+const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[0]]);
+temp.forEach((res) => {
+  data.push(res);
+});
+
+// Printing data
+console.log(data);
+
 // const fs = require('fs');
 // const initSqlJs = require('sql-wasm.js');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 980,
-    height: 600
-  })
+    height: 600,
+  });
 
-  win.loadFile('map.html')
-}
+  win.loadFile("map.html");
+};
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+});
 
-initSqlJs().then(function(SQL){
-    // Load the db
-    const db = new SQL.Database();
-    let sqlstr = "CREATE TABLE hello (a int, b char); \
+initSqlJs().then(function (SQL) {
+  // Load the db
+  const db = new SQL.Database();
+  let sqlstr =
+    "CREATE TABLE hello (a int, b char); \
     INSERT INTO hello VALUES (0, 'hello'); \
     INSERT INTO hello VALUES (1, 'world');";
-    db.run(sqlstr);
+  db.run(sqlstr);
 
-    const stmt = db.prepare("SELECT * FROM hello WHERE a=:aval AND b=:bval");
+  const stmt = db.prepare("SELECT * FROM hello WHERE a=:aval AND b=:bval");
 
-    // Bind values to the parameters and fetch the results of the query
-    const result = stmt.getAsObject({':aval' : 1, ':bval' : 'world'});
-    console.log(result); // Will print {a:1, b:'world'}
-  });
+  // Bind values to the parameters and fetch the results of the query
+  const result = stmt.getAsObject({ ":aval": 1, ":bval": "world" });
+  console.log(result); // Will print {a:1, b:'world'}
+});
