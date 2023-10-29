a64113135f501ceb7cc3b49f2e240c58844daff4
Aristarchos Tzatzos <djaris97@hotmail.com>
2023-10-30 01:19:46 +0200

    Data insertion working

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const path = require("node:path");
 
 let filebuffer = fs.readFileSync("./database.db");
+let db;
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -29,7 +30,7 @@ app.whenReady().then(() => {
 
 initSqlJs().then(function (SQL) {
   // Load the db
-  const db = new SQL.Database(filebuffer);
+  db = new SQL.Database(filebuffer);
 
   // const stmt = db.prepare('SELECT * FROM "Complex"');
   // while (stmt.step()) console.log(stmt.get());
@@ -39,10 +40,53 @@ function insertData(filePath) {
   let file = reader.readFile(filePath);
   let data = [];
 
+  const columnNames = reader.utils.sheet_to_json(
+    file.Sheets[file.SheetNames[0]],
+    { header: 1 }
+  )[0];
+
   const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[0]]);
   temp.forEach((res) => {
     data.push(res);
   });
 
-  console.log(data);
+  const insertPersonSQL = `
+      INSERT INTO "Person" (surname, name, am, department, phone, phoneSecondary, mail, room) 
+      VALUES (:surname, :name, :am, :department, :phone, :phoneSecondary, :mail, :room);
+    `;
+
+  const insertRoomSQL = `
+      INSERT INTO "Room" (id, number, belongsToComplex, belongsToBuilding, type, phone, lan) 
+      VALUES (:id, :number, :belongsToComplex, :belongsToBuilding, :type, :phone, :lan);
+    `;
+
+  const stmtPerson = db.prepare(insertPersonSQL);
+  const stmtRoom = db.prepare(insertRoomSQL);
+
+  for (const line of data) {
+    stmtRoom.bind({
+      ":id": line[columnNames[0]],
+      ":number": line[columnNames[3]],
+      ":belongsToComplex": Math.floor((line[columnNames[0]] - 1) / 60) + 1,
+      ":belongsToBuilding": Math.floor((line[columnNames[0]] - 1) / 30) + 1,
+      ":type": line[columnNames[4]],
+      ":phone": String(line[columnNames[5]]),
+      ":lan": line[columnNames[6]],
+    });
+    stmtRoom.step();
+    stmtRoom.reset();
+
+    stmtPerson.bind({
+      ":surname": line[columnNames[7]],
+      ":name": line[columnNames[8]],
+      ":am": String(line[columnNames[9]]),
+      ":department": line[columnNames[10]],
+      ":phone": String(line[columnNames[11]]),
+      ":phoneSecondary": String(line[columnNames[12]]),
+      ":mail": line[columnNames[13]],
+      ":room": line[columnNames[0]],
+    });
+    stmtPerson.step();
+    stmtPerson.reset();
+  }
 }
